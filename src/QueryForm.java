4ae8219a68bdd4b80e1080f252b891c3a704215d
ctammes <ctammes@gmail.com>
2013-03-04 07:03:08 +0100

    initieel

---
+++ src/QueryForm.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.jar.JarInputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chris
+ * Date: 1-3-13
+ * Time: 16:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueryForm {
+    private JTextField txtFilenaam;
+    private JButton btnLees;
+    private JPanel queryPanel;
+    private JTextField txtCategorie;
+    private JTextField txtTitel;
+    private JTextArea txtTekst;
+    private JButton btnOpslaan;
+
+
+    public QueryForm() {
+        btnLees.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                File queryFile = new File(txtFilenaam.getText());
+                JOptionPane.showMessageDialog(null, queryFile, "Info", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
+
+    private void leesFile(File file) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            String regel = in.readLine();
+            while (regel != null) {
+                System.out.println(regel);
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("QueryForm");
+        frame.setContentPane(new QueryForm().queryPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
